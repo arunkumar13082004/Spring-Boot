dc3efe4745cb94dc750610d353803d675b4b059c
Arunkumar <arunkumarjagadi04@gmail.com>
2024-09-22 14:56:36 +0530

    Spring Boot first commit

---
+++ IntroductionToBoot/src/main/java/com/_To100/Arun/week1/IntroductionToBoot/IntroductionToBootApplication.java
@@ -0,0 +1,13 @@
+package com._To100.Arun.week1.IntroductionToBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToBootApplication.class, args);
+	}
+
+}
---
+++ IntroductionToBoot/src/test/java/com/_To100/Arun/week1/IntroductionToBoot/IntroductionToBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com._To100.Arun.week1.IntroductionToBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
